33cebe8ee20adac7c80793046c8dbc9aa75bc2f4
MikeMarshall36 <90028680+MikeMarshall36@users.noreply.github.com>
2021-09-23 12:41:57 +0300

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,17 @@
+import socket
+from time import sleep
+
+sock = socket.socket()
+sock.setblocking(1)
+sock.connect(('127.0.0.1', 9091))
+
+msg = input()
+while len(msg) != 0:
+    sock.send(msg.encode())
+    data = sock.recv(1024)
+    exit_check = msg.lower()
+    if exit_check == 'exit':
+        sock.close()
+        break
+    print(data.decode())
+    msg = input()
