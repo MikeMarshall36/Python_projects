1b39ab69717b0fc476f3bf0608b287fc2f71d96f
Сергей Витальевич Королев <90028680+MikeMarshall36@users.noreply.github.com>
2023-02-02 21:18:29 +0300

    Final version
    Реализовал все необходимые функции и все работает как ожидается!

--- Task_1/Lark.py
+++ Task_1/Lark.py
@@ -13,60 +13,54 @@ from pandas import Series
 operator_list = ['and', 'or', 'not']  # Лист с операторами
 query_input = str(input("Enter query: "))  # Ввод запроса
 
-# Ниже представлена грамматика для парсера.
-"""
-Проблема возникла именно с грамматикой, так как ESCAPED_STRING и WORD работают не совсем так, как ожидал
-Суть в том, что при попытке использовать самописные правила с регулярными выражениями, Lark их как будто игнорирует
-Пробовал по разному, но в итоге уже потратил несколько дней и каких-либо изменений не увидел.
-Сейчас грамматика переварит практически все, в том числе и не правильные варианты
-В сети смотрел примеры использования, но их очень мало 
-"""
 query_grammar = r"""
-    ?start: query
+    ?start: (query)
 
-    ?query: key_word
+    query: (key_word
     | key_phrase
-    | operator // Данные, которые мы встретим в запросе
-    key_word : ESCAPED_STRING | WORD // Описание ключевого слова (может быть в кавычках, а может и без)
-    key_phrase : key_word + // Описание ключевой фразы (Всегда обрамляется кавычками) 
-    operator :  and | not | or // Операторы
-    and : /"and"/
-    not : /"not"/
-    or : /"or"/
-    
-// Импорты из стандарты шаблонов библиотеки 
-   %import common.ESCAPED_STRING // Строка с кавычками
-   %import common.WORD // Просто слово
+    | operator
+    | key_word_q)*
 
-   %ignore " " // Игнорировать пробел
+    key_word_q : /"[^"]\S*"/
+    key_word : WORD
+    key_phrase : /"[^"]*"/
+    operator : and | not | or
+    and : "and"
+    not : "not"
+    or : "or"
+
+   %import common.ESCAPED_STRING
+   %import common.WORD
+
+   %ignore " "
 """
 
 
-# Функция для предобработки запроса
+# Функция для предварительной обработки запроса
 def _preprocessor_(text: str) -> str:
     text = text.lower()  # Переносим запрос в нижний регистр
     text = text.replace('"', '\"').replace('(', '').replace(')', '')
     # Убираем скобки и заменяем кавычки на то, что может увидеть Lark
-    text = text.split(' ')  # Режем строку по пробелам
-    preprocessed_query = []  # Создаем список для обработанного запроса
-    for word in text:
-        if word not in operator_list:
-            preprocessed_query.append(word)  # Записываем все значения, что не входят в операторы
+    preprocessed_query = text.split(' ')  # Режем строку по пробелам
     processed_text = str(' '.join(preprocessed_query))  # Создаем строку из списка запроса без операторов
     return processed_text
 
-
 def get_keywords(query: str) -> list:  # Функция для поиска ключевых слов
     result_list = []  # Список с результатами
-    processed_query = _preprocessor_(query)  # Вызываем обработчик запроса
-    query_parser = Lark(query_grammar)  # Создаем парсер с использованием нашей грамматики
-    tree = query_parser.parse(processed_query)  # Создаем дерево результатов
-    tree = tree.pretty().replace('key_phrase', '').replace('key_word\t', '').split('\n')  # Переводим дерево в список
-    for i in range(len(tree)):
-        if tree[i] != '' and tree[i] != ' ':  # Ищем слова и фразы
-            result_list.append(tree[i].strip())  # Записываем в список
-    result_list = Series(result_list)  # Конвертируем список в серию, чтобы вытащить уникальные значения
-    result_list.unique()  # В общем вытаскиваем уникальные значения
+    query_parser = Lark(query_grammar, parser="earley", ambiguity="explicit")
+    # Вызываем парсер, чтобы получить данные
+    try:
+        processed_query = _preprocessor_(query)  # Вызываем обработчик запроса
+        tree = query_parser.parse(processed_query)
+        result_tree = tree.pretty().split('query')[-1]
+        # print(result_tree) # Вывод итогового варианта обработанного запроса (раскомментируйте, чтобы увидеть дерево)
+        for word in result_tree.split('\n'):  # Избавляемся от переноса каретки
+            if word.strip().startswith('key'):  # Ищем строки начинающиеся на key
+                result_list.append(word.strip().split('\t')[-1])  # Берем значения для ключевых элементов
+        result_list = Series(result_list)  # Конвертируем список в серию, чтобы вытащить уникальные значения
+        result_list.unique()  # В общем вытаскиваем уникальные значения
+    except UnexpectedInput:
+        return query.split(' ')  # В случае если запрос не прошел проверку правил - выводим список с исходным запросом
     return list(result_list)
 
 
@@ -80,6 +74,5 @@ def check_query(query: str) -> bool:  # Проверяем грамматику 
     return True
 
 
-# print(_preprocessor_(query_input))
-print(get_keywords(query_input))
-print(check_query(query_input))
+print(get_keywords(query_input))  # Вывод ключевых слов
+print(check_query(query_input))  # Вывод результата проверки
